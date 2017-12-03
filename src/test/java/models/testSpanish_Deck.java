6822a22c28dd502593e704421ac17faa39f8ae31
duli4 <duli@oregonstate.edu>
2017-12-02 17:31:08 -0800

    check spanish deck

---
+++ src/test/java/models/testSpanish_Deck.java
@@ -0,0 +1,16 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testSpanish_Deck{
+
+  @Test
+  public void testGetSpanishDeck(){
+
+    Spanish_Deck deck2 = new Spanish_Deck();
+    deck2.shuffle();
+    assertEquals(deck2.size(),50);
+  }
+}
